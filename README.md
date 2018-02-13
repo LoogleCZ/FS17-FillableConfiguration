9a695517fa327f1371c0d584c4f8d97628dbeb91
Martin Fabík <mar.fabik@gmail.com>
2018-02-14 00:12:48 +0100

    Added configurable fillUnits

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Fillable configuration for FS17
 
 Script is under development! If you want to help me, let me know first.
+
+If you'll use this specialization, some validity checks of GE scripts will not work, so remember "Fillable needs to be the first specialization which implements setIsFilling"
--- src/FillableConfiguration.lua
+++ src/FillableConfiguration.lua
@@ -14,6 +14,9 @@
 -- This is development version! DO not use it on release!
 --
 
+-- register new configuration type
+ConfigurationUtil.registerConfigurationType("fillConf", "Fillable configuration", nil, nil, nil, ConfigurationUtil.SELECTOR_MULTIOPTION);
+
 FillableConfiguration = {};
 
 function FillableConfiguration.prerequisitesPresent(specializations)
@@ -24,59 +27,114 @@ function FillableConfiguration.prerequisitesPresent(specializations)
 	return true;
 end;
 
-function FillableConfiguration.loadConfPre(...)
-	print("Configuration preload");
-	print(tostring(self));
-	if arg ~= nil and type(arg) == "table" then
-		for k,v in pairs(arg) do
-			print(k .. "=>" .. v);
-		end;
-	end;
-	print("Configuration preload end;");
-end;
-
-function FillableConfiguration.loadConfPost(...)
-	print("Configuration postload");
-	print(tostring(self));
-	if arg ~= nil and type(arg) == "table" then
-		for k,v in pairs(arg) do
-			print(k .. "=>" .. v);
-		end;
-	end;
-	print("Configuration postload end;");
-end;
-
-function FillableConfiguration.loadConfSingleItem(...)
-	print("Configuration item load");
-	print(tostring(self));
-	if arg ~= nil and type(arg) == "table" then
-		for k,v in pairs(arg) do
-			print(k .. "=>" .. v);
-		end;
-	end;
-	print("Configuration item load end;");
-end;
-
-ConfigurationUtil.registerConfigurationType("fillConf", "Fillable configuration", FillableConfiguration.loadConfPre, FillableConfiguration.loadConfSingleItem, FillableConfiguration.loadConfPost, ConfigurationUtil.SELECTOR_MULTIOPTION);
 
 function FillableConfiguration:preLoad(savegame)
 end;
 
 function FillableConfiguration:load(savegame)
-	self.LFO = {};
+	-- replace all data loaded by Fillable in this load. Update, Draw, etc...
+	-- will be left on fillable
 	
-	--
-	--	for saving using self.configurations["fillConf"]
-	-- and 
-	--	self.boughtConfigurations["fillConf"]
-	--
+	print("Configurations");
+	DebugUtil.printTableRecursively(self.configurations, "-", 0 ,4);
 	
-	DebugUtil.printTableRecursively(ConfigurationUtil, "", 0, 4 );
+	local key, fillConfId = Vehicle.getXMLConfigurationKey(self.xmlFile, self.configurations["fillConf"], "vehicle.fillConfConfigurations.fillConfConfiguration", "vehicle", "fillConf");
+	print(tostring(key));
+    local fallbackConfigKey = "vehicle.fillConfConfigurations.fillConfConfiguration(0)";
+    local fallbackOldKey = "vehicle";
+
+	self.supportsFillTriggers = Vehicle.getConfigurationValue(self.xmlFile, key, ".supportsFillTriggers", "#value", getXMLBool, self.supportsFillTriggers, fallbackConfigKey, fallbackOldKey);
+
+	if self.supportsFillTriggers then
+        self.setIsFilling = Fillable.setIsFilling;
+        self.addFillTrigger = Fillable.addFillTrigger;
+        self.removeFillTrigger = Fillable.removeFillTrigger;
+        self.fillLitersPerSecond = Vehicle.getConfigurationValue(self.xmlFile, key, ".fillLitersPerSecond", "", getXMLFloat, 500, fallbackConfigKey, fallbackOldKey);
+        local unitFillTime = Vehicle.getConfigurationValue(self.xmlFile, key, ".unitFillTime", "", getXMLFloat, nil, fallbackConfigKey, fallbackOldKey);
+        if unitFillTime ~= nil then
+            self.unitFillTime = unitFillTime * 1000;
+        end
+        self.currentFillTime = 0;
+        self.fillTriggers = {};
+        self.fillActivatable = FillActivatable:new(self);
+        self.isFilling = false;
+    end;
+	
+	self.fillTypeChangeThreshold = Vehicle.getConfigurationValue(self.xmlFile, key, ".fillTypeChangeThreshold", "", getXMLFloat, 0.05, fallbackConfigKey, fallbackOldKey);
+	
+	self.fillUnits = {};
+	local unitsBase = fallbackOldKey .. ".fillUnits.fillUnit";
+	if key ~= nil and hasXMLProperty(self.xmlFile, fallbackConfigKey..".fillUnits.fillUnit(0)") then
+        unitsBase = fallbackConfigKey..".fillUnits.fillUnit";
+    end
+    if key ~= nil and hasXMLProperty(self.xmlFile, key..".fillUnits.fillUnit(0)") then
+        unitsBase = key..".fillUnits.fillUnit";
+    end
+    local i=0;
+    while true do
+        local keyUnits = string.format(unitsBase .. "(%d)", i);
+        if not hasXMLProperty(self.xmlFile, keyUnits) then
+            break;
+        end
+		print(tostring(keyUnits));
+        local entry = {};
+        entry.fillUnitIndex = i+1;
+        entry.fillVolumeIndex = Utils.getNoNil(getXMLInt(self.xmlFile, keyUnits .. "#fillVolumeIndex"), entry.fillUnitIndex);
+        entry.currentFillType = FillUtil.FILLTYPE_UNKNOWN;
+        entry.lastValidFillType = FillUtil.FILLTYPE_UNKNOWN;
+        entry.fillLevel = 0;
+        if self.isServer then
+            entry.sentFillType = entry.currentFillType;
+            entry.sentFillLevel = entry.fillLevel;
+        end
+        entry.capacity = getXMLFloat(self.xmlFile, keyUnits .. "#capacity");
+        entry.unit = getXMLString(self.xmlFile, keyUnits .. "#unit");
+        entry.showOnHud = Utils.getNoNil(getXMLBool(self.xmlFile, keyUnits .. "#showOnHud"), true);
+        entry.fillTypes = {};
+        local fillTypes = {};
+        local fillTypeCategories = getXMLString(self.xmlFile, keyUnits .. "#fillTypeCategories");
+        local fillTypeNames = getXMLString(self.xmlFile, keyUnits .. "#fillTypes");
+        if fillTypeCategories ~= nil and fillTypeNames == nil then
+            fillTypes = FillUtil.getFillTypeByCategoryName(fillTypeCategories, "Warning: '"..self.configFileName.. "' has invalid fillTypeCategory '%s'.")
+        elseif fillTypeCategories == nil and fillTypeNames ~= nil then
+            fillTypes = FillUtil.getFillTypesByNames(fillTypeNames, "Warning: '"..self.configFileName.. "' has invalid fillType '%s'.")
+        else
+            print("Warning: '"..self.configFileName.. "' a fillUnit entry needs either the 'fillTypeCategories' or 'fillTypes' attribute.")
+        end
+        if fillTypes ~= nil then
+            for _,fillType in pairs(fillTypes) do
+                entry.fillTypes[fillType] = true;
+            end
+        end
+        entry.lastFillLevel = 0;
+        entry.fillLevelHud = VehicleHudUtils.loadHud(self, self.xmlFile, "fillLevel", nil, i);
+        table.insert(self.fillUnits, entry);
+        i=i+1;
+    end;
+	-- we don't have fallback, because modders should use new config style
+	
+	print("load done");
 	
 	-- end of loading
 end;
 
 function FillableConfiguration:postLoad(savegame)
+	print("postload");
+	DebugUtil.printTableRecursively(self.fillUnits, "-", 0, 4);
+	-- need to be loaded here because in time when Fillable is loaded there is no fillunit
+	if savegame ~= nil and not savegame.resetVehicles and self.synchronizeFillLevels then
+        if hasXMLProperty(savegame.xmlFile, savegame.key.."#fillLevels") and hasXMLProperty(savegame.xmlFile, savegame.key.."#fillTypes") then
+            local fillLevels = { Utils.getVectorFromString(Utils.getNoNil(getXMLString(savegame.xmlFile, savegame.key.."#fillLevels"), "")) };
+            local fillTypes = Utils.splitString(" ", Utils.getNoNil(getXMLString(savegame.xmlFile, savegame.key.."#fillTypes"), ""));
+            for i,fillType in pairs(fillTypes) do
+                if fillLevels[i] ~= nil then
+                    local fillTypeInt = FillUtil.fillTypeNameToInt[fillType];
+                    self:setUnitFillLevel(i, fillLevels[i], fillTypeInt, false);
+                end;
+            end;
+        end;
+    end;
+	print(tostring(self:getFillLevel()));
 end;
 
 function FillableConfiguration:getSaveAttributesAndNodes(nodeIdent)
@@ -85,6 +143,7 @@ end;
 function FillableConfiguration:delete() end;
 
 function FillableConfiguration:update(dt)
+	print(tostring(self:getFillLevel()));
 end;
 
 function FillableConfiguration:updateTick(dt)
